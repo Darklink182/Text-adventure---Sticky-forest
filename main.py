f72cc92fa712683e93f1f6262e16a685583b60f9
Julian <110328@st-maartenscollege.nl>
2020-06-15 08:04:02 +0000

    Title

--- main.py
+++ main.py
@@ -1,1 +1,54 @@
+import time
+import os
+import sys
+import cursor
+
+os.system('clear')
+
+time.sleep(2)
+
+print("\t**********************************************")
+print("\t***  Hi there! - Welcome to Sticky Forest  ***")
+print("\t**********************************************")
+
+time.sleep(1)
+print('Before we begin, we should probably introduce ourselves!')
+time.sleep(1)
+print(' ')
+time.sleep(1)
+print('Sticky Forest is a text based adventure game')
+time.sleep(1)
+print('This project was made on assignment for dhr. Logtenberg')
+time.sleep(1)
+print('For the subject Informatica at Sint-Maartenscollege, Voorburg')
+time.sleep(1)
+print(' ')
+time.sleep(1)
+print('A production by')
+print('Daniël Sylvester Prévoo & Julian Bakx')
+time.sleep(1)
+for e in range(5):
+  time.sleep(.5)
+  print(' ')
+
+
+print("To start the game type 'start'")
+
+
+
+while True:
+ d = input()
+ if d == "start":
+   print("Have fun!")
+   time.sleep(.2)
+   os.system("python SP.py")
+   break
+ else:
+   print("That's not an option! Try again!")
+   continue
+
+
+
+
+
 
